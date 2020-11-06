61198cbaf8518dae3a2dd06882754c1ddac3cdb7
nishaque <61164463+nishaque@users.noreply.github.com>
2020-11-06 12:10:04 +0300

    Update CacheInfo.cpp

--- sources/CacheInfo.cpp
+++ sources/CacheInfo.cpp
@@ -3,83 +3,81 @@
 //
 
 #include "CacheInfo.hpp"
+const int intsize = 4;
+const size_t numIter = 1000;
+const int zero = 0;
+const size_t cacheLine = 16;
 
-std::vector<int> cacheInfo::generateCacheInfo() {
-  std::vector<int> sizeOfBuf = std::vector<int>();
+CacheInfo::CacheInfo() {
+  sizeOfBuf = std::vector<int>();
   for (int i = 64; i <= 4096; i *= 2) {
-    sizeOfBuf.push_back(i * 4);
+    sizeOfBuf.push_back(i * intsize);
   }
-  sizeOfBuf.push_back(2048 * 3 * 4);
-  return sizeOfBuf;
+  sizeOfBuf.push_back(2048 * 3 * intsize);
+  k = 0;
 }
 
-int cacheInfo::straightExp(int Size) {
-  int k;
+int CacheInfo::straightExp(int Size) {
   int *arr = new int[Size];
-  for (int i = 0; i < Size; i += 16)  // прогрев
+  for (int i = zero; i < Size; i += cacheLine)  // прогрев
     k = arr[i];
   std::chrono::microseconds base =
       std::chrono::duration_cast<std::chrono::microseconds>(
           std::chrono::system_clock::now().time_since_epoch());
-  for (int t = 0; t < 1000; t++) {
-    for (int i = 0; i < Size; i += 16)  // чтение
+  for (size_t t = zero; t < numIter; t++) {
+    for (int i = zero; i < Size; i += cacheLine)  // чтение
       k = arr[i];
   }
   std::chrono::microseconds delta =
       std::chrono::duration_cast<std::chrono::microseconds>(
           std::chrono::system_clock::now().time_since_epoch()) -
       base;
-  arr[1] = k;
   return delta.count();
 }
 
-int cacheInfo::reverseExp(int Size) {
-  int k;
+int CacheInfo::reverseExp(int Size) {
   int *arr = new int[Size];
-  for (int i = Size - 1; i >= 0; i -= 16)  // прогрев
+  for (int i = Size - 1; i >= 0; i -= cacheLine)  // прогрев
     k = arr[i];
   std::chrono::microseconds base =
       std::chrono::duration_cast<std::chrono::microseconds>(
           std::chrono::system_clock::now().time_since_epoch());
-  for (int t = 0; t < 1000; t++) {
-    for (int i = Size - 1; i >= 0; i -= 16)  // чтение
+  for (size_t t = zero; t < numIter; t++) {
+    for (int i = Size - 1; i >= 0; i -= cacheLine)  // чтение
       k = arr[i];
   }
   std::chrono::microseconds delta =
       std::chrono::duration_cast<std::chrono::microseconds>(
           std::chrono::system_clock::now().time_since_epoch()) -
       base;
-  arr[1] = k;
   return delta.count();
 }
 
-int cacheInfo::randomExp(int size) {
-  int k;
+int CacheInfo::randomExp(int size) {
   std::vector<int> indOrder = std::vector<int>();
-  for (int i = 0; i < size; i += 16) {
+  for (int i = zero; i < size; i += cacheLine) {
     indOrder.push_back(i);
   }
   auto random = std::default_random_engine{};
   std::shuffle(std::begin(indOrder), std::end(indOrder), random);
 
   int *arr = new int[size];
-  for (size_t i = 0; i < indOrder.size(); i++)  // прогрев
+  for (size_t i = zero; i < indOrder.size(); i++)  // прогрев
     k = arr[indOrder[i]];
   std::chrono::microseconds base =
       std::chrono::duration_cast<std::chrono::microseconds>(
           std::chrono::system_clock::now().time_since_epoch());
-  for (int t = 0; t < 1000; t++) {
-    for (size_t i = 0; i < indOrder.size(); i++) k = arr[indOrder[i]];
+  for (int t = zero; t < numIter; t++) {
+    for (size_t i = zero; i < indOrder.size(); i++) k = arr[indOrder[i]];
   }
   std::chrono::microseconds delta =
       std::chrono::duration_cast<std::chrono::microseconds>(
           std::chrono::system_clock::now().time_since_epoch()) -
       base;
-  arr[1] = k;
   return delta.count();
 }
 
-int cacheInfo::investigate(std::string &method, int size) {
+int CacheInfo::investigate(std::string &method, int size) {
   int r;
   if (method == "straight") {
     r = straightExp(size);
@@ -92,38 +90,37 @@ int cacheInfo::investigate(std::string &method, int size) {
   }
   return r;
 }
-void cacheInfo::printHeader(std::string &method) {
+void CacheInfo::printHeader(std::string &method) {
   std::cout << "investigation:" << '\n';
   std::cout << "  travel_variant: " << method << '\n';
   std::cout << "  experiments:" << '\n';
 }
 
-void cacheInfo::printResult(int size, int durat, int number) {
+void CacheInfo::printResult(int size, int durat, int number) {
   std::cout << "  - experiment:" << '\n';
   std::cout << "  number:" << number + 1 << '\n';
   std::cout << "  input_data:" << '\n';
   if (size > 4095)
     std::cout << "    buffer_size: " << size / 4096 << "mb" << '\n';
   else
-    std::cout << "    buffer_size: " << size / 4 << "kb" << '\n';
+    std::cout << "    buffer_size: " << size / intsize << "kb" << '\n';
   std::cout << "  results:" << '\n';
   std::cout << "    duration: " << durat << " microseconds" << '\n';
 }
 
-void cacheInfo::printReport(std::string &method, std::vector<int> &cacheInfo) {
+void CacheInfo::printReport(std::string &method) {
   printHeader(method);
-  for (size_t i = 0; i < cacheInfo.size(); i++) {
-    printResult(cacheInfo[i], investigate(method, cacheInfo[i]), i);
+  for (size_t i = zero; i < sizeOfBuf.size(); i++) {
+    printResult(sizeOfBuf[i], investigate(method, sizeOfBuf[i]), i);
   }
 }
 
-void cacheInfo::printReports() {
-  std::vector<int> caches = generateCacheInfo();
+void CacheInfo::printReports() {
   std::vector<std::string> methods = std::vector<std::string>();
   methods.emplace_back("straight");
   methods.emplace_back("reverse");
   methods.emplace_back("random");
   for (std::string s : methods) {
-    printReport(s, caches);
+    printReport(s);
   }
 }
